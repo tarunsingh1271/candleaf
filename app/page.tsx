3b89ba3d209fead03a7dfef2f7a4c75083103885
tarunsingh1271 <tarun.singh@edu.devinci.fr>
2023-11-28 00:31:36 +0100

    few things changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image'
+import Navbar from './pages/navigation'
 
 export default function Home() {
   return (
+  
     <main>
-      
-            
+      <Navbar />
     </main>
+    
+    
   )
 }
