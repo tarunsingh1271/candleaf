9958edf8d6d5740a262e8339a8d56dfd28102f24
tarunsingh1271 <tarun.singh@edu.devinci.fr>
2023-11-28 02:26:41 +0100

    few changes tried navbar insertion

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
 import Image from 'next/image'
 import Navbar from './pages/navigation'
+import About from './pages/about'
 
 export default function Home() {
   return (
+  <>
   
-    <main>
-      <Navbar />
-    </main>
+  
+  <main>
+    
+  <Navbar />
+  </main>
+  </>
+    
+    
     
     
   )
--- app/pages/navigation/index.js
+++ app/pages/navigation/index.js
@@ -1,34 +1,32 @@
 
 import React from 'react'
+import Link from 'next/link'
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+
 
 const Navbar = () => {
 
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">Candleaf</a>
+    <nav className="navbar">   
 
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Discovery</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">About</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">Contact </a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-    </div>
+      <Link className="navbar-brand" href="/">Candleaf</Link>
+
+      <button className="navbar" type="button"></button>
+
+      
+          <Link className="nav-link active" href="/">Discovery</Link>
+      
+          <Link className="nav-link" href="/about">About</Link>
+
+          <Link className="nav-link" href="/contactus">Contact Us</Link>      
+
+    </nav>
+    
   )
 }
+
+
 
 export default Navbar
